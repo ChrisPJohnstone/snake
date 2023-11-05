446e23b329f8b6ed1d683b38ceede7774c13ca92
Christopher Johnstone <chrispjohnstone@gmail.com>
2023-11-05 11:13:56 +0000

    added steps to draw gameboard

--- cpp/app.cpp
+++ cpp/app.cpp
@@ -1,7 +1,44 @@
+// required header file 
+#include <conio.h>
 #include <iostream>
+#include <windows.h>
+
 using namespace std;
 
-int main() {
-    cout << "Hello World";
+bool gameOver;
+
+const int height = 20;
+const int width = 20;
+const char wallChar = '#';
+
+void Setup(){
+    gameOver=false;
+}
+void Draw(){
+    system("CLS");
+    for (int row = 0; row < height; row++) {
+        for (int column = 0; column < width; column++) {
+            if (row == 0 || row == height - 1) {
+                cout << wallChar;
+            } else if (column == 0 || column == width - 1) {
+                cout << wallChar;
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+void Input(){}
+void Logic(){}
+
+int main(){
+    Setup();
+    while (!gameOver){
+        Draw();
+        Input();
+        Logic();
+    }
     return 0;
 }
+
