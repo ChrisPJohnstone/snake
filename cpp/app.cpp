8b907156a16fe885e7b4db87a58bdb98483a511f
Christopher Johnstone <chrispjohnstone@gmail.com>
2023-11-05 11:24:46 +0000

    Added steps to recognise character input

--- cpp/app.cpp
+++ cpp/app.cpp
@@ -6,13 +6,34 @@
 using namespace std;
 
 bool gameOver;
+const char wallChar = '#';
+const char snakeHeadChar = '0';
+const char fruitChar = 'F';
 
 const int height = 20;
 const int width = 20;
-const char wallChar = '#';
+
+int snakeX, snakeY, fruitX, fruitY, score;
+
+enum eDirection {
+    STOP=0,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+};
+const char dirUpChar = 'w';
+const char dirDownChar = 's';
+const char dirLeftChar = 'a';
+const char dirRightChar = 'd';
 
 void Setup(){
     gameOver=false;
+    eDirection = STOP;
+    snakeX = width / 2;
+    snakeY = height / 2;
+    fruitX = rand() %width;
+    fruitY = rand() %height;
 }
 void Draw(){
     system("CLS");
@@ -22,6 +43,10 @@ void Draw(){
                 cout << wallChar;
             } else if (column == 0 || column == width - 1) {
                 cout << wallChar;
+            } else if (row == snakeY && column == snakeX) {
+                cout << snakeHeadChar;
+            } else if (row == fruitY && column == fruitX) {
+                cout << fruitChar;
             } else {
                 cout << " ";
             }
@@ -29,8 +54,27 @@ void Draw(){
         cout << endl;
     }
 }
-void Input(){}
-void Logic(){}
+void Input(){
+    if (_kbhit()) {
+        switch(_getch()) {
+        case dirUpChar:
+            eDirection = UP;
+            break;
+        case dirDownChar:
+            eDirection = DOWN;
+            break;
+        case dirLeftChar:
+            eDirection = LEFT;
+            break;
+        case dirRightChar:
+            eDirection = RIGHT;
+            break;
+        }
+    }
+}
+void Logic(){
+    
+}
 
 int main(){
     Setup();
@@ -38,6 +82,7 @@ int main(){
         Draw();
         Input();
         Logic();
+        Sleep(40);
     }
     return 0;
 }
